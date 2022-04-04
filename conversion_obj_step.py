3757594cf87f42ec3c026c410b8795301d12a7f6
Jess Frazelle <github@jessfraz.com>
2022-04-04 12:49:22 -0700

    update
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- conversion_obj_step.py
+++ conversion_obj_step.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 from kittycad.client import ClientFromEnv
 from kittycad.models import FileConversion, ValidSourceFileFormat, ValidOutputFileFormat
 from kittycad.api.file import post_file_conversion_with_base64_helper
