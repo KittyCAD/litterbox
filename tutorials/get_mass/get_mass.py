ef271bb7ecd52e049bc8b12c31dd6c1fe0b8dd90
Kurt Hutten IrevDev <k.hutten@protonmail.ch>
2022-06-14 12:26:44 +1000

    start of python tutorials for mass and volume
    Though there might be a problem with the skd

---
+++ tutorials/get_mass/get_mass.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+from kittycad.client import ClientFromEnv
+from kittycad.models import file_source_format
+from kittycad.api.file import create_file_mass
+from kittycad.models import FileMass
+
+# Create a new client with your token parsed from the environment variable:
+#   KITTYCAD_API_TOKEN.
+client = ClientFromEnv()
+
+# Read in the contents of the file.
+file = open("./ORIGINALVOXEL-3.obj", "rb")
+content = file.read()
+file.close()
+
+print(file_source_format.FileSourceFormat.OBJ)
+
+fm: FileMass = create_file_mass.sync(
+    client=client,
+    material_density=5.0,
+    src_format=file_source_format.FileSourceFormat.OBJ,
+    body=content
+)
+
+print(f"File mass: {fm}")
---
+++ tutorials/get_volume/get_volume.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+from kittycad.client import ClientFromEnv
+from kittycad.models import file_source_format
+from kittycad.api.file import create_file_volume
+from kittycad.models import FileMass
+
+# Create a new client with your token parsed from the environment variable:
+#   KITTYCAD_API_TOKEN.
+client = ClientFromEnv()
+
+# Read in the contents of the file.
+file = open("./ORIGINALVOXEL-3.obj", "rb")
+content = file.read()
+file.close()
+
+print(file_source_format.FileSourceFormat.OBJ)
+
+fv: FileMass = create_file_volume.sync(
+    client=client,
+    src_format=file_source_format.FileSourceFormat.OBJ,
+    body=content
+)
+
+print(f"File volume: {fv}")
