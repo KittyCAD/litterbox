1786ab99f08499b668f822c7098608196e332836
Kurt Hutten IrevDev <k.hutten@protonmail.ch>
2022-09-20 16:05:07 +1000

    initial samples

---
+++ samples/file_mass/file_mass.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"github.com/kittycad/kittycad.go"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func main() {
+	// Create a new client with your token parsed from the environment variable:
+	// KITTYCAD_API_TOKEN.
+	client, _ := kittycad.NewClientFromEnv("your apps user agent")
+
+	input, _ := os.Open("./ORIGINALVOXEL-3.obj")
+	content, _ := ioutil.ReadAll(bufio.NewReader(input))
+	// Encode as base64.
+	massReader := strings.NewReader(base64.StdEncoding.EncodeToString(content))
+
+	densitySteelGramsPerCubicMillimeter := 0.00784
+
+	fc, _ := client.File.CreateMass(
+		densitySteelGramsPerCubicMillimeter,
+		"obj",
+		massReader,
+	)
+
+	fmt.Println("File mass (g): ", fc.Mass)
+
+	json_data, _ := json.Marshal(struct {
+		Title  string  `json:"title"`
+		Mass   float64 `json:"mass"`
+	}{
+		Title:  "output.json",
+		Mass:   fc.Mass,
+	})
+
+	output_file_path := "./output.json"
+	output, _ := os.Create(output_file_path)
+	defer output.Close()
+	output.Write(json_data)
+	if err := output.Sync(); err != nil {
+		panic(err)
+	}
+	exec.Command("cp", "./ORIGINALVOXEL-3.obj", "./output.obj").Output()
+}
