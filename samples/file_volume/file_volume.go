a411aab501baaac3759300014d6b3b39221174ac
Kurt Hutten <k.hutten@protonmail.ch>
2022-09-21 12:30:59 +1000

    add volume sample (#61)
    * add volume sample

---
+++ samples/file_volume/file_volume.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/kittycad/kittycad.go"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	// Create a new client with your token parsed from the environment variable:
+	// KITTYCAD_API_TOKEN.
+	client, _ := kittycad.NewClientFromEnv("your apps user agent")
+
+	fileBytes, _ := os.ReadFile("./ORIGINALVOXEL-3.obj")
+
+	fc, _ := client.File.CreateVolume(
+		"obj",
+		fileBytes,
+	)
+
+	fmt.Println("File volume: ", fc.Volume)
+
+	json_data, _ := json.Marshal(struct {
+		Title  string  `json:"title"`
+		Volume float64 `json:"volume"`
+	}{
+		Title:  "output.json",
+		Volume: fc.Volume,
+	})
+
+	output_file_path := "./output.json"
+	output, _ := os.Create(output_file_path)
+	defer output.Close()
+	output.Write(json_data)
+	if err := output.Sync(); err != nil {
+		panic(err)
+	}
+	exec.Command("cp", "./ORIGINALVOXEL-3.obj", "./output.obj").Output()
+}
