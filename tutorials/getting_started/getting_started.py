7f9d6a131c5aaf8c35c60fb7c291aa58d5d97e8c
Kurt Hutten <k.hutten@protonmail.ch>
2022-09-27 20:50:12 +1000

    add getting started and ping examples (#68)
    * add getting started and ping examples
    
    * Updating script outputs
    
    * tests
    
    * tests
    
    * Updating script outputs
    
    * remove go test
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ tutorials/getting_started/getting_started.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+from kittycad.client import ClientFromEnv
+from kittycad.models import file_output_format, file_source_format, file_conversion
+from kittycad.api.file import create_file_conversion_with_base64_helper
+import json
+
+# Create a new client with your token parsed from the environment variable:
+#   KITTYCAD_API_TOKEN.
+client = ClientFromEnv()
+
+# Convert a file from OBJ to STL.
+# Read in the contents of the file.
+file = open("./ORIGINALVOXEL-3.obj", "rb")
+# LITTERBOX-END-NON-EDITABLE-SECTION
+content = file.read()
+file.close()
+
+fc: file_conversion.FileConversion = create_file_conversion_with_base64_helper.sync(
+    client=client,
+    body=content,
+    src_format=file_source_format.FileSourceFormat.OBJ,
+    output_format=file_output_format.FileOutputFormat.STL)
+
+print(f"File conversion id: {fc.id}")
+# Try adding your name by changing the text below to
+# "File conversion status for <your-name>":
+print(f"File conversion status: {fc.status}")
+
+# LITTERBOX-START-NON-EDITABLE-SECTION
+output_file_path = "./output.stl"
+print(f"Saving output to {output_file_path}")
+output_file = open(output_file_path, "wb")
+output_file.write(fc.output)
+output_file.close()
