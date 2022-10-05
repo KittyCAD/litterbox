dbcdebaae7655fd26f6e958c87b85dc22ed2b9ea
Kurt Hutten IrevDev <k.hutten@protonmail.ch>
2022-10-05 13:29:26 +1100

    Update go version to add go center of mass sample

---
+++ samples/file_center_of_mass/file_center_of_mass.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/kittycad/kittycad.go"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	// Create a new client with your token parsed from the environment variable:
+	// KITTYCAD_API_TOKEN.
+	client, _ := kittycad.NewClientFromEnv("your apps user agent")
+
+	fileBytes, _ := os.ReadFile("./ORIGINALVOXEL-3.obj")
+	// LITTERBOX-END-NON-EDITABLE-SECTION
+
+	densitySteelGramsPerCubicMillimeter := 0.00784
+
+	fc, err := client.File.CreateCenterOfMass(
+		densitySteelGramsPerCubicMillimeter,
+		"obj",
+		fileBytes,
+	)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Print(fc)
+	fmt.Println("File center of mass: ", fc.CenterOfMass)
+
+	json_data, _ := json.Marshal(struct {
+		Title string  `json:"title"`
+		CenterOfMass []float64 `json:"center_of_mass"`
+	}{
+		Title: "output.json",
+		CenterOfMass:  fc.CenterOfMass,
+	})
+
+	// LITTERBOX-START-NON-EDITABLE-SECTION
+	output_file_path := "./output.json"
+	output, _ := os.Create(output_file_path)
+	defer output.Close()
+	output.Write(json_data)
+	if err := output.Sync(); err != nil {
+		panic(err)
+	}
+	exec.Command("cp", "./ORIGINALVOXEL-3.obj", "./output.obj").Output()
+}
