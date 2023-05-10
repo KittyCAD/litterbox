0ab3f51d0d17ae5a9c7bb106e2d81a20e4a4b734
Kurt Hutten Irev-Dev <k.hutten@protonmail.ch>
2023-05-10 12:26:34 +1000

    remove other obj_nomtl intances

--- tutorials/getting_started/getting_started.py
+++ tutorials/getting_started/getting_started.py
@@ -19,7 +19,7 @@ file.close()
 fc = create_file_conversion_with_base64_helper.sync(
     client=client,
     body=content,
-    src_format=FileImportFormat.OBJ_NOMTL,
+    src_format=FileImportFormat.OBJ,
     output_format=FileExportFormat.STL)
 
 if isinstance(fc, Error) or fc == None:
