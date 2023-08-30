0c850e82063c78a570edfba534f80fa96215d940
Jess Frazelle <github@jessfraz.com>
2023-08-30 16:16:26 -0700

    updates
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- samples/convert_file/convert_file.py
+++ samples/convert_file/convert_file.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 
 
+from typing import Dict
+
 from kittycad.api.file import create_file_conversion
 from kittycad.client import ClientFromEnv
+from kittycad.models.base64data import Base64Data
 from kittycad.models.error import Error
 from kittycad.models.file_conversion import FileConversion
 from kittycad.models.file_export_format import FileExportFormat
 from kittycad.models.file_import_format import FileImportFormat
+from kittycad.types import Unset
 
 # Create a new client with your token parsed from the environment variable:
 #   KITTYCAD_API_TOKEN.
@@ -21,8 +25,8 @@ file.close()
 
 result = create_file_conversion.sync(
     client=client,
-    output_format=FileExportFormat.STL,
     src_format=FileImportFormat.OBJ,
+    output_format=FileExportFormat.STL,
     body=content,
 )
 
@@ -34,11 +38,16 @@ fc: FileConversion = result
 print(f"File conversion id: {fc.id}")
 print(f"File conversion status: {fc.status}")
 
-if fc.outputs.length != 1:
+if isinstance(fc.outputs, Unset):
+    raise Exception("Expected outputs to be set")
+
+outputs: Dict[str, Base64Data] = fc.outputs
+
+if len(outputs) != 1:
     raise Exception("Expected one output file")
 
 # LITTERBOX-START-NON-EDITABLE-SECTION
-for _, output in fc.outputs.items():
+for _, output in outputs.items():
     output_file_path = "./output.stl"
     print(f"Saving output to {output_file_path}")
     output_file = open(output_file_path, "wb")
