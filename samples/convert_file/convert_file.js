b0d3466090c0fcd99a89a909373bdbb2e4cc5dba
Pierre Jacquier <pierrejacquier39@gmail.com>
2023-09-06 09:07:24 -0400

    `output` on file conversions is deprecated (#135)
    * `output` on file conversions is deprecated
    Fixes #132
    
    * Change to looping over results
    
    * Clean up
    
    * Clean up
    
    * Make it more go like
    
    * Clean up
    
    * Clean up

--- samples/convert_file/convert_file.js
+++ samples/convert_file/convert_file.js
@@ -5,18 +5,23 @@ async function main() {
     const body = await fsp.readFile('./ORIGINALVOXEL-3.obj', 'base64')
     // LITTERBOX-END-NON-EDITABLE-SECTION
     
-    const response = await file.create_file_conversion({
+    const fc = await file.create_file_conversion({
         output_format: 'stl',
         src_format: 'obj',
         body,
     })
-    if ('error_code' in response) throw response
-    const { status, id, output } = response
-    console.log(`File conversion id: ${id}`)
-    console.log(`File conversion status: ${status}`)
+    if ('error_code' in fc) throw fc
+    console.log(`File conversion id: ${fc.id}`)
+    console.log(`File conversion status: ${fc.status}`)
+
+    const outputs = Object.entries(fc.outputs)
+    if (outputs.length !== 1) throw Error("Expected one output file")
     
     // LITTERBOX-START-NON-EDITABLE-SECTION
-    await fsp.writeFile('./output.stl' , output, 'base64')
+    for (const [_, output] of outputs) {
+        const outputFilePath = './output.stl'
+        await fsp.writeFile(outputFilePath, output, 'base64')
+    }
 }
 
 main()
