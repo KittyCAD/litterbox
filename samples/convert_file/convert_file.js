097297824209368dadac7166eb174e26e69534ad
Pierre Jacquier <pierrejacquier39@gmail.com>
2023-09-06 06:53:42 -0400

    Clean up

--- samples/convert_file/convert_file.js
+++ samples/convert_file/convert_file.js
@@ -5,19 +5,22 @@ async function main() {
     const body = await fsp.readFile('./ORIGINALVOXEL-3.obj', 'base64')
     // LITTERBOX-END-NON-EDITABLE-SECTION
     
-    const response = await file.create_file_conversion({
+    const fc = await file.create_file_conversion({
         output_format: 'stl',
         src_format: 'obj',
         body,
     })
-    if ('error_code' in response) throw response
-    const { status, id, outputs } = response
-    console.log(`File conversion id: ${id}`)
-    console.log(`File conversion status: ${status}`)
+    if ('error_code' in fc) throw fc
+    console.log(`File conversion id: ${fc.id}`)
+    console.log(`File conversion status: ${fc.status}`)
+
+    const outputs = Object.entries(fc.outputs)
+    if (outputs.length !== 1) throw Error("Expected one output file")
     
     // LITTERBOX-START-NON-EDITABLE-SECTION
-    for (const [fileName, fileContents] of Object.entries(outputs)) {
-        await fsp.writeFile(`./${fileName}`, fileContents, 'base64')
+    for (const [_, output] of outputs) {
+        const outputFilePath = './output.stl'
+        await fsp.writeFile(outputFilePath, output, 'base64')
     }
 }
 
