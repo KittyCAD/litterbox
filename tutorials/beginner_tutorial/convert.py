e24f391858ae8f8be0fce721d6f3a44c0c1930c9
Josh Gomez <114548659+jgomez720@users.noreply.github.com>
2024-01-18 15:26:02 -0700

    cleaning tutorials (#180)
    * cleaning tutorials
    
    * Updating script outputs
    
    * update python script
    
    * LOOK ON MY REFORMAT, YE MIGHTY, AND DESPAIR!
    
    * Updating script outputs
    
    * update the convert.py script with more comments
    
    * LOOK ON MY REFORMAT, YE MIGHTY, AND DESPAIR!
    
    * Updating script outputs
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- tutorials/beginner_tutorial/convert.py
+++ tutorials/beginner_tutorial/convert.py
@@ -9,30 +9,38 @@ from kittycad.models.file_import_format import FileImportFormat
 from kittycad.types import Unset
 
 
-# Create a new client with your token parsed from the environment variable
-# KITTYCAD_API_TOKEN
-def convertCubetoSTL():
+# Create a new function to convert an OBJ file to an STL file.
+def convertOBJtoSTL():
+    # Create a new client with your token parsed from the environment variable (KITTYCAD_API_TOKEN)
     client = ClientFromEnv(timeout=500, verify_ssl=True)
-    # Convert a file from OBJ to STL.
+
     # Read in the contents of the file.
-    file = open("./cube.obj", "rb")
+    file = open("./dodecahedron.obj", "rb")
     content = file.read()
     file.close()
+
+    # Call the create_file_conversion function with the required parameters: client, body, src_format, and output_format.
+
     result: Optional[Union[Error, FileConversion]] = create_file_conversion.sync(
-        client=client,
-        body=content,
-        src_format=FileImportFormat.OBJ,
-        output_format=FileExportFormat.STL,
+        client=client,  # The client you created above.
+        body=content,  # The contents of the file you read in.
+        src_format=FileImportFormat.OBJ,  # The format of the file you read in.
+        output_format=FileExportFormat.STL,  # The format you want to convert to.
     )
+
+    # Check if the result is an error or None.
     if isinstance(result, Error) or result is None:
         raise Exception("There was a problem")
+
     body: FileConversion = result
     if isinstance(body.outputs, Unset):
         raise Exception("Expected outputs to be set")
+
+    # Loop through the outputs and save them to a file.
     outputs: Dict[str, Base64Data] = body.outputs
 
     for _, output in outputs.items():
-        output_file_path = "./output.stl"
+        output_file_path = "./dodecahedron.stl"
         print(f"Saving output to {output_file_path}")
         output_file = open(output_file_path, "wb")
         output_file.write(output.get_decoded())
@@ -40,4 +48,4 @@ def convertCubetoSTL():
     return body
 
 
-convertCubetoSTL()
+convertOBJtoSTL()
