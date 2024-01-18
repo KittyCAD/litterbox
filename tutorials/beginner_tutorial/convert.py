22bcd97e9d0f34812acfbd9197ee662b4cfa2b1b
jgomez720 <114548659+jgomez720@users.noreply.github.com>
2024-01-18 12:43:34 -0700

    update the convert.py script with more comments

--- tutorials/beginner_tutorial/convert.py
+++ tutorials/beginner_tutorial/convert.py
@@ -8,27 +8,35 @@ from kittycad.models.file_export_format import FileExportFormat
 from kittycad.models.file_import_format import FileImportFormat
 from kittycad.types import Unset
 
-
-# Create a new client with your token parsed from the environment variable
-# KITTYCAD_API_TOKEN
+# Create a new function to convert an OBJ file to an STL file.
 def convertOBJtoSTL():
+
+    # Create a new client with your token parsed from the environment variable (KITTYCAD_API_TOKEN)
     client = ClientFromEnv(timeout=500, verify_ssl=True)
-    # Convert a file from OBJ to STL.
+    
     # Read in the contents of the file.
     file = open("./dodecahedron.obj", "rb")
     content = file.read()
     file.close()
+
+    # Call the create_file_conversion function with the required parameters: client, body, src_format, and output_format.
+
     result: Optional[Union[Error, FileConversion]] = create_file_conversion.sync(
-        client=client,
-        body=content,
-        src_format=FileImportFormat.OBJ,
-        output_format=FileExportFormat.STL,
+        client=client, # The client you created above.
+        body=content, # The contents of the file you read in.
+        src_format=FileImportFormat.OBJ, # The format of the file you read in.
+        output_format=FileExportFormat.STL, # The format you want to convert to.
     )
+
+    # Check if the result is an error or None.
     if isinstance(result, Error) or result is None:
         raise Exception("There was a problem")
+    
     body: FileConversion = result
     if isinstance(body.outputs, Unset):
         raise Exception("Expected outputs to be set")
+    
+    # Loop through the outputs and save them to a file.
     outputs: Dict[str, Base64Data] = body.outputs
 
     for _, output in outputs.items():
@@ -38,6 +46,5 @@ def convertOBJtoSTL():
         output_file.write(output.get_decoded())
         output_file.close()
     return body
-
 
 convertOBJtoSTL()
