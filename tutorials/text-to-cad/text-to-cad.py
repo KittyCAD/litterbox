0669093fb7c3ef3ece4fd46d7662a964ef71279c
Greg Sweeney <greg@kittycad.io>
2024-02-14 13:59:03 -0500

    update text-to-cad tutorial

--- tutorials/text-to-cad/text-to-cad.py
+++ tutorials/text-to-cad/text-to-cad.py
@@ -1,68 +1,46 @@
 import time
-from typing import Any, List, Optional, Tuple, Union
 
 from kittycad.api.ai import create_text_to_cad, get_text_to_cad_model_for_user
 from kittycad.client import ClientFromEnv
-from kittycad.models import Error, TextToCad
+from kittycad.models.api_call_status import ApiCallStatus
 from kittycad.models.file_export_format import FileExportFormat
 from kittycad.models.text_to_cad_create_body import TextToCadCreateBody
-from kittycad.types import Response
 
+# Create our client.
+client = ClientFromEnv()
 
-def text_to_cad_gen():
+# Prompt the API to generate a 3D model from text.
+response = create_text_to_cad.sync(
+    client=client,
+    output_format=FileExportFormat.STEP,
+    body=TextToCadCreateBody(
+        prompt="Design a gear with 40 teeth",
+    ),
+)
 
-    # Create our client.
-    client = ClientFromEnv()
+# Polling to check if the task is complete
+while response.completed_at is None:
+    # Wait for 5 seconds before checking again
+    time.sleep(5)
 
-    # Call the ML-ephant API to generate a 3D model from text.
-    result: Optional[Union[TextToCad, Error]] = create_text_to_cad.sync(
+    # Check the status of the task
+    response = get_text_to_cad_model_for_user.sync(
         client=client,
-        output_format=FileExportFormat.GLTF,
-        body=TextToCadCreateBody(
-            prompt="design a 40 tooth gear", # The prompt to use for generating the model
-        ),
+        id=response.id,
     )
 
-    # Check if the response is an error
-    if isinstance(result, Error) or result == None:
-        print(result)
-        raise Exception("Error in response")
+if response.status == ApiCallStatus.FAILED:
+    # Print out the error message
+    print(f"Text-to-CAD failed: {response.error}")
 
-    # Print the response
-    body: TextToCad = result
-    print(body)
- 
-    # Get the ID from the response
-    task_id = body.id
+elif response.status == ApiCallStatus.COMPLETED:
+    # Print out the names of the generated files
+    print(f"Text-to-CAD completed and returned {len(response.outputs)} files:")
+    for name in response.outputs:
+        print(f"  * {name}")
 
-    # Polling to check if the task is complete
-    while True:
-        # Check the status of the task
-        check_result: Optional[
-            Union[TextToCad, Error]
-        ] = get_text_to_cad_model_for_user.sync(
-        client=client,
-        id=task_id,
-    )
-
-        if isinstance(check_result, Error):
-            print(check_result)
-            raise Exception("Error checking task status")
-
-        if check_result.completed_at != None:  # Check to see if completed_at is not None (meaning, it's done)
-            break
-
-        # Wait for 5 seconds before checking again
-        time.sleep(5) 
-
-    # Retrieve the final result
-    final_result = check_result.outputs["source.gltf"] # Get the GLTF file from the outputs
-
-    output_file_path = "./text-to-cad-output.gltf"
-    print(f"Saving output to {output_file_path}")
-    output_file = open(output_file_path, "wb") 
-
-    output_file.write(final_result.get_decoded()) # Write the GLTF information to the text-to-cad-output.gltf file
-    output_file.close()
-
-text_to_cad_gen()
+    # Save the STEP data as text-to-cad-output.step
+    final_result = response.outputs["source.step"]
+    with open("text-to-cad-output.step", "w", encoding="utf-8") as output_file:
+        output_file.write(final_result.get_decoded().decode("utf-8"))
+        print(f"Saved output to {output_file.name}")
